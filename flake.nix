35c1dea208452bdbbaf7e47e02fb7f08eb42b7cf
Izzy Lancaster <avrisaac555@gmail.com>
2023-07-27 18:24:29 -0700

    Added overlay for ruff

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
             inputs.nixpkgs.follows = "nixpkgs";
         };
   };
-    outputs = inputs@{flake-parts, ...}:
+    outputs = inputs@{nixpkgs, flake-parts, ...}:
         flake-parts.lib.mkFlake { inherit inputs; } {
             imports = [
                 inputs.devshell.flakeModule
@@ -24,6 +24,24 @@
                 inherit (poetry2nix) mkPoetryEnv;
             in
             {
+                # Defining nixpkgs overlays
+                _module.args.pkgs = import nixpkgs {
+                    inherit system;
+                    overlays = [
+                        # Overlay for ruff to use the latest version
+                        (final: prev: {
+                            ruff = prev.ruff.overrideAttrs (old: rec {
+                                version = "0.0.280";
+                                src = prev.fetchFromGitHub {
+                                    owner = "astral-sh";
+                                    repo = "ruff";
+                                    rev = "v${version}";
+                                    hash = "sha256-Pp/yurRPUHqrCD3V93z5EGMYf4IyLFQOL9d2sNe3TKs=";
+                                };
+                            });
+                        })
+                    ];
+                };
                 devshells.default = {
                     packages = [
                         pkgs.sqlite
